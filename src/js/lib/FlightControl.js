84f683d3ea9e6364ea3dda0e4c5590e2480840d6
Alberto Estrada Puerta <albertoe@gmail.com>
2015-02-27 15:45:20 -0500

    Refactored FlightControl js

--- src/js/lib/FlightControl.js
+++ src/js/lib/FlightControl.js
@@ -1,14 +1,23 @@
 var $ = require('jquery');
-    require('store-js');
+require('store-js');
 
-var endPoints = {
-        destinations : "https://flightcontrol.io/api/routes/destinations",
-        countries : "https://flightcontrol.io/api/routes/countries",
-        regions : "https://flightcontrol.io/api/routes/regions",
-    },
-    expiration = 86400000,
-    lang = 'es';
+var defaults = {
+        lang: 'es',
+        api: {
+            destinations : "https://flightcontrol.io/api/routes/destinations",
+            countries : "https://flightcontrol.io/api/routes/countries",
+            regions : "https://flightcontrol.io/api/routes/regions",
+        },
+        storageExpiration: 86400000,
+        storage: true,
+    }
+;
 
+/**
+ * Extension to the storage class
+ * to setup the expiration value
+ * @type {Object}
+ */
 var storeWidthExpiration = {
     set: function(key, val, exp) {
         store.set(key, { val:val, exp:exp, time:new Date().getTime() })
@@ -21,33 +30,48 @@ var storeWidthExpiration = {
     }
 }
 
-
+/**
+ * Module FlightControl
+ */
 class FlightControl {
 
-    constructor() {
+    constructor(options) {
+
+        this.options = $.extend({}, defaults, options);
+        this._defaults = defaults;
+
         if(!store.enabled) {
             console.log('browser not supported or in private mode');
-
+            this.options.storage = false;
         }
     }
 
-
+    /**
+     * Fetch data from flight controller
+     * based on the resource name
+     * @param  {string}   resourceName: destinations|countries|regions
+     * @param  {Function} cb  callback
+     * @return {Function} callback
+     */
     fetch(resourceName, cb) {
         var resourceValue = {};
 
-        if(storeWidthExpiration.get(resourceName) && storeWidthExpiration.get(resourceName + '.count')) {
+        if(this.options.storage && storeWidthExpiration.get(resourceName)
+           && storeWidthExpiration.get(resourceName + '.count')) {
             resourceValue.list = storeWidthExpiration.get(resourceName);
             resourceValue.count = storeWidthExpiration.get(resourceName + '.count');
 
            return cb(resourceValue);
         }
 
-        $.getJSON(endPoints[resourceName], (data) => {
+        $.getJSON(this.options.api[resourceName], (data) => {
 
             this.sortNames(data);
 
-            storeWidthExpiration.set(resourceName, data, this.expiration);
-            storeWidthExpiration.set(resourceName + '.count', data.length, this.expiration);
+            if(this.options.storage) {
+                storeWidthExpiration.set(resourceName, data, this.options.storageExpiration);
+                storeWidthExpiration.set(resourceName + '.count', data.length, this.options.storageExpiration);
+            }
             resourceValue.list = data;
             resourceValue.count = data.length;
 
@@ -55,14 +79,11 @@ class FlightControl {
         });
     }
 
-    //to be defined
-    api() {
-        // this.fetch(function(data){
-        //     console.log(data);
-        // }, 'regions');
-    }
-
-
+    /**
+     * Helper function to sort data
+     * based on language
+     * @param  {Object} data
+     */
     sortNames(data) {
         data.sort(function(a, b) {
             if (a.name[lang] > b.name[lang]) return 1;
