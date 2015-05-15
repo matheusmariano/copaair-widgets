4d44933d491826bbf918c05a7923b61839481cd0
Alberto Estrada Puerta <albertoe@gmail.com>
2015-05-15 19:17:30 -0400

    Updated signup widget

--- src/js/widgets/Signup.js
+++ src/js/widgets/Signup.js
@@ -16,7 +16,46 @@ class Signup {
             'lang': this.options.lang,
             callback: (html) => {
                 this.$form.html(html);
+                this.signupEvents();
             }
+        });
+    }
+
+
+
+    signupEvents() {
+        var $form = this.$form;
+        var $toggle = this.$form.find('.js-copaair-toggle');
+
+        // Show bottom row when any input gets focus
+        $form.on('focus.copaair', 'input', function(e) {
+            $form.addClass('copaair-widget-open');
+            $toggle.removeClass('copaair-hidden');
+        });
+
+        $form.on('submit', (e) => {
+            e.preventDefault();
+            this.submitForm(e.target);
+        })
+
+    }
+
+    submitForm(target) {
+        var $form = $(target);
+
+        var data = $form.serializeObject();
+        data.fullname = data.first_name + ' ' + data.last_name;
+        data.source = this.options.source;
+        data.language = this.options.lang;
+
+        var container = this.options.container;
+
+        $.ajax({
+            type: 'POST',
+            url: 'https://flightcontrol.io/api/signup/add',
+            data: data
+        }).done(function(res) {
+            container.fadeOut();
         });
     }
 
