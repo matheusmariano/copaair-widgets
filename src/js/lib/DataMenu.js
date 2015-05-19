92962cc4f1e3bb92268a3e8cfb34aba50d306a19
Alberto Estrada Puerta <albertoe@gmail.com>
2015-05-19 19:30:25 -0400

    Added temporary implementation to signup form

---
+++ src/js/lib/DataMenu.js
@@ -0,0 +1,71 @@
+var $ = require('jquery'),
+    FlightControl = require('./FlightControl'),
+
+    defaults = {
+        lang: 'es',
+        contentType: 'countries',
+        callback: function() {}
+    }
+;
+
+
+
+class DataMenu
+{
+    /**
+     * Constructor
+     * @param  {Object} options Custom options for this widget instance.
+     */
+    constructor(options) {
+        var defaults = {
+            lang: 'es',
+        };
+
+        this.options = $.extend({}, defaults, options);
+
+        this.setup();
+    }
+
+    setup() {
+        var flightControl = new FlightControl({ lang: this.options.lang });
+
+        flightControl.fetch(this.options.contentType, (data) => {
+            // Format raw destinations to autocomplete structure
+            this.options.source = this.format(data.list);
+            this.render();
+            if (typeof cb === 'function') {
+                cb();
+            }
+        });
+    }
+
+    render() {
+        $.each(this.options.source, (i, item) => {
+            $(this.options.selector).append(item.display);
+        });
+
+    }
+
+    /**
+     * Formats data into the needed structure to be displayed
+     * on the autocomplete menu widget.
+     * @param  {Array} destinations Raw data returned from Flight Control
+     * @return {Array}              Formatted destinations
+     */
+    format(list) {
+        var result = [];
+
+        $.each(list, (i, item) => {
+            let option =
+                `<option value="${ item.id }">${ item.name[this.options.lang] }</option>`;
+            result.push({
+                display:option
+            });
+        });
+
+        return result;
+    }
+
+}
+
+module.exports = DataMenu;
--- src/js/widgets/Signup.js
+++ src/js/widgets/Signup.js
@@ -1,4 +1,7 @@
-var Template = require('../lib/Template');
+var Template = require('../lib/Template'),
+    DataMenu = require('../lib/DataMenu'),
+    i18n = require('../../../lang/datepicker.json')
+;
 
 class Signup {
 
@@ -6,7 +9,9 @@ class Signup {
         this.$form = $(element);
 
         var defaults = {
-            lang: 'es'
+            lang: 'es',
+            widgetPosition: { my: 'left bottom', at: 'left top' },
+
         };
 
         this.options = $.extend({}, defaults, options);
@@ -17,11 +22,48 @@ class Signup {
             callback: (html) => {
                 this.$form.html(html);
                 this.signupEvents();
+
+                this.$form.find('.js-selectmenu').each( function() {
+                    var dataMenu = new DataMenu({
+                        lang: this.options.lang,
+                        contentType: $(this).data('content'),
+                        selector: $(this)
+                    });
+                });
+
+                this.setupSelectMenus();
+
+                $('.js-signup-date').datepicker({
+                    changeMonth: true,
+                    changeYear: true,
+                    format: 'dd/mm/yy',
+                    beforeShow: function(input, isnt) {
+                        setTimeout(function() {
+                            isnt.dpDiv.position({
+                                my: 'left bottom',
+                                at: 'left top',
+                                of: input
+                            });
+                        }, 0);
+                    }
+                });
+
+                var regional = i18n[this.options.lang].regional;
+                $.datepicker.regional;
             }
         });
     }
 
+    /**
+     * Replaces select menus with custom UI widgets
+     */
+    setupSelectMenus() {
+        $('.js-selectmenu').selectmenu({
+            position: this.options.widgetPosition
+        });
 
+        return this;
+    }
 
     signupEvents() {
         var $form = this.$form;
@@ -38,6 +80,18 @@ class Signup {
             this.submitForm(e.target);
         })
 
+        $('.js-country-selector').selectmenu({
+            change: (e, ui) => {
+                this.options.country = ui.item.value;
+            }
+        });
+
+        $('.js-city-selector').selectmenu({
+            change: (e, ui) => {
+                this.options.city = ui.item.value;
+            }
+        });
+
     }
 
     submitForm(target) {
@@ -46,7 +100,9 @@ class Signup {
         var data = $form.serializeObject();
         data.fullname = data.first_name + ' ' + data.last_name;
         data.source = this.options.source;
-        data.language = this.options.lang;
+        data.language = this.options.lang.toUpperCase();
+        data.city = this.options.city;
+        data.country = this.options.country;
 
         var container = this.options.container;
 
